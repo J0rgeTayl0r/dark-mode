bd38b627611f5af95c442441601b06221121a0bc
carlo rothschild <carlorothschild@thors-hammer.home>
2020-05-06 19:16:32 -0400

    MVP

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,9 +1,9 @@
 import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = (key, initialValue) => {
-  const [isDark, setDark] = useLocalStorage('isDark', false);
-  const page = window.document.page;
-  isDark ? page.classList.add('dark-mode') : page.classList.remove('dark-mode');
+export const useDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorage('isDark', false);
+  const page = window.document.body;
+  isDarkMode ? page.classList.add('dark-mode') : page.classList.remove('dark-mode');
     
-         return  [isDark , setDark]
+         return  [isDarkMode , setIsDarkMode]
        };
